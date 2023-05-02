d93d51094e2beb7b8c9ab423d967a96c46a35d28
Nicolas Bergmann <bergmann@fhi-berlin.mpg.de>
2023-05-02 09:48:10 +0000

    Created the MCRunner class

--- mcrunner/run_mc.py
+++ mcrunner/run_mc.py
@@ -19,144 +19,217 @@ from multiprocessing import Pool
 
 
 
+class MCRunner():
 
-def build_initial_struc(primstruct, size, ads_species, init_type):
-    structure = make_supercell(primstruct,
-                       1 * np.array([[size, 0, 0],
-                                     [0, size, 0],
-                                     [0, 0, 1]]))
-    n_species = len(ads_species)
-    occ = []
-    for x in range(size):
-        for y in range(size):
-            if init_type == 'c2x2':
-                tmp = (x+y) % 2
-                if tmp ==0:
-                    occ.append(0)
+
+    def __init__(self):
+        """
+        """
+        self.batch_kwargs = {}
+        self.mc_args = {}
+        return None
+
+    def _update_mc_args(self):
+
+        mc_arg_keys = ['init_type', 'ads_species', 'size', 'temperature', 
+                       'steps', 'prob_threshold'
+                       ]
+        for key in self.batch_kwargs:
+            if key in mc_arg_keys:
+                self.mc_args[key] = self.batch_kwargs.pop(key)
+        return None
+
+    @property
+    def mc_args(self):
+        return self.mc_args
+    
+    @mc_args.setter
+    def mc_args(self, kwargs : dict):
+        self.mc_kwargs.update(kwargs)
+    
+    @property
+    def batch_kwargs(self):
+        return self.batch_kwargs
+
+    @batch_kwargs.setter
+    def batch_kwargs(self, kwargs : dict):
+        self.batch_kwargs.update(kwargs)
+        self._update_mc_args()
+
+    @staticmethod
+    def build_initial_struc(CE,
+                            size : int,
+                            ads_species : list,
+                            init_type : str
+                            ):
+        """
+        Builds the initial ase structure from prompts given by the user
+
+        Inputs 
+        ======
+        size: int
+            Size of the square lattice in x and y dimension
+        ads_species: list
+            List of chemical symbols that can adsorb on the structure
+            The order of the strings matter, last one should always be 'X'
+        init_type: str
+            Keyword that indicates how the structure should look
+            Allowed keywords are: 'c2x2', 'bare', 'full', 'random'
+        """
+        # Get the primitive structure from the Cluster Expansion
+        primstruct = CE.get_cluster_space_copy().primitive_structure
+
+        structure = make_supercell(primstruct,
+                        1 * np.array([[size, 0, 0],
+                                        [0, size, 0],
+                                        [0, 0, 1]]))
+        n_species = len(ads_species)
+        # Determines how the sites get occupied
+        occ = []
+        for x in range(size):
+            for y in range(size):
+                if init_type == 'c2x2':
+                    tmp = (x+y) % 2
+                    if tmp ==0:
+                        occ.append(0)
+                    else:
+                        occ.append(1)
+                elif init_type == 'bare':
+                    occ.append(n_species-1)
+                elif init_type == 'full':
+                        occ.append(0)
                 else:
-                    occ.append(1)
-                #occ.append((x+y) % n_species)
-            elif init_type == 'bare':
-                occ.append(n_species-1)
-            elif init_type == 'full':
-                    occ.append(0)
-            else:
-                occ.append(np.random.randint(n_species))
+                    occ.append(np.random.randint(n_species))
 
-    structure.set_chemical_symbols([ads_species[o] for o in occ ])
-    return structure
-
-def read_initial_struc(filename):
-    structure = read(filename)
-    return structure
+        structure.set_chemical_symbols([ads_species[o] for o in occ ])
+        return structure
+    
+    @staticmethod
+    def read_initial_struc(filename : str):
+        """
+        Gets the initial structure for the Monte Carlo simulation from an
+        ase readable file
+        """
+        structure = read(filename)
+        return structure
 
 
-def run_single_mc(args):
-    ce_file     = args['ce_file']
-    ce = ClusterExpansion.read(ce_file)
+    @staticmethod
+    def run_single_mc(
+            ce_file : str,
+            out_file : str,
+            delta_mu : list,
+            from_read : bool = False,
+            strucname : str = "",
+            **mc_args
+        ):
+        """
+        Runs a Monte Carlo Simulation on a single Core
+        
+        Inputs
+        ======
+        ce_file, str
+            Path to a mchammer readable file to a Cluster Expansion
+        out_file, str
+            Path to the mchammer output file
+        delta_mu, list of floats,
+            Reference chemical potentials of the adsorbed species,
+            the length of the list must be as long as in the same order as the
+            ads_species
+        from_read, bool (default = False)
+            Should the initial structure be read from an ase readable file
+            if True requires additional kwarg strucname
+        """
+        ce = ClusterExpansion.read(ce_file)
 
-    init_type   = args['init_type']
-    ads_species = args['ads_species']
-    size        = args['size']
+        ads_species = mc_args.get('ads_species')
+        delta_mu    = mc_args.get('dmu')
+        chemical_potentials = {
+            ads: delta_mu[i] for i, ads in enumerate(ads_species)
+        }
 
-    primstruct  = ce.get_cluster_space_copy().primitive_structure
-    structure   = build_initial_struc(primstruct, size, ads_species, init_type)
-    #structure   = read_initial_struc(strucname)
-    calculator = ClusterExpansionCalculator(structure, ce)
-
-    out_file    = args['out_file']
-    temperature = args['temperature']
-    dmu         = args['dmu']
-    steps       = args['steps']
-
-    chemical_potentials = {
-        ads_species[i]: dmu[i] for i in range(len(ads_species))}
-
-    mc = SemiGrandCanonicalEnsemble_Diffsteps(
-        structure=structure, calculator=calculator,
-        temperature=temperature, dc_filename=out_file,
-        chemical_potentials=chemical_potentials
-    )
-    mc.prob_threshold = args['prob_threshold']
-    mc.run(number_of_trial_steps=steps)
-    return None
-
-
-def run_batch_mcs(**kwargs):
-
-    out_dir = kwargs.get("out_dir")
-    # Make sure output directory exists
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-
-    mc_args = {
-        'init_type':            kwargs.pop('init_type'),
-        'ads_species':          kwargs.pop('ads_species'),
-        'size':                 kwargs.pop('size'),
-        'temperature':          kwargs.pop('temperature', 300.),
-        'steps':                kwargs.pop('steps'),
-        'prob_threshold':       kwargs.pop('prob_threshold')
-    }
-    log_file = kwargs.pop('log_file', 'log.json')
-
-    ce_df = kwargs.pop("ce_df")
-
-    count = 0
-    n_repeats       = kwargs.pop('n_repeats', 1)
-    pot             = kwargs.pop("potential")
-    args = []
-    for i in range(n_repeats):
-        for j in ce_df.index:
-            # Here we also want to go up to 0.001 V resolution on the x axis
-            row = cp(ce_df.loc[j])
-            mc_run_args = cp(mc_args)
-            mc_run_args['ce_file']  = row['filename']
-            mc_run_args['out_file'] = f'{out_dir}/run_{count}.dc'
-            mc_run_args['dmu']      = [pot, pot, 0.]
-            mc_run_args['repeat']   = i
-            mc_run_args['file_idx'] = count
-            mc_run_args['ref']      = row['ref']
-            mc_run_args['pot']      = row['pot']
-
-            with open(log_file, 'a') as f:
-                json.dump(mc_run_args, f)
-                f.write('\n')
-            args.append(mc_run_args)
-            count += 1
-
-    # step 4: Define a Pool object with the desired number of processes and run
-    processes = kwargs.pop('processes', 20)
-    pool = Pool(processes=processes)
-    results = pool.map_async(run_single_mc, args)
-    results.get()
-    return None
+        if from_read:
+            structure   = MCRunner.read_initial_struc(strucname)
+        else:
+            size        = mc_args.get('size')
+            init_type   = mc_args.get('init_type')
+            structure   = MCRunner.build_initial_struc(
+                    ce, size, ads_species, init_type
+            )
+        
+        calculator = ClusterExpansionCalculator(structure, ce)
+        
+        mc = SemiGrandCanonicalEnsemble_Diffsteps(
+            structure=structure,
+            calculator=calculator,
+            temperature=mc_args.get('temperature'),
+            dc_filename=out_file,
+            chemical_potentials=chemical_potentials,
+            prob_threshold=mc_args.get('prob_threshold')
+        )
+        mc.run(
+            number_of_trial_steps=mc_args.get('steps')
+        )
+        return None
 
 
-def main():
-    n_cores     = 30
-    n_steps     = 10**6
-    init_type   = "bare"
+    def run_batch_mcs(self,
+                      ce_df : pd.DataFrame,
+                      delta_mu: list,
+                      n_cores : int
+        ):
+        """
+        Organizes the parallel submission of multiple Monte Carlo simulations
 
-    df = pd.read_json(f'./CE_at_applied_potential/log.json', lines=True)
+        Inputs 
+        ======
+        ce_df, pd.DataFrame (required columns: 'filename', 'ref', 'pot')
+            DataFrame containing the paths to the cluster expansion files
+            and the reference electrode and potential they were measured for
+        delta_mu: list,
+            List of floats as long as the ads_species in the cluster expansion
+            We assume here that this value doesn't change throughout all 
+            monte carlo runs, meaning that all necessary experimental 
+            parameters have already been set during the cluster expansion fit
+        n_cores, int
+            Number of cores over which multiprocessing Pools over
+        """
+       
+        out_dir = self.batch_kwargs.pop("out_dir")
+        # Make sure output directory exists
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+            
+        log_file = self.batch_kwargs.pop(
+            'log_file', os.path.join(out_dir, 'log.json')
+        )
+        if os.path.isfile(log_file):
+            raise ValueError("log file already exists! Aborting now...")
 
-    out_dir     = f"./run"
+        count           = 0
+        n_repeats       = self.batch_kwargs.get('n_repeats')
+        args = []
+        for i in range(n_repeats):
+            for j in ce_df.index:
+                # Here we also want to go up to 0.001 V resolution on the x axis
+                row = cp(ce_df.loc[j])
+                mc_run_args = cp(self.mc_args)
+                mc_run_args['ce_file']  = row['filename']
+                mc_run_args['out_file'] = f'{out_dir}/run_{count}.dc'
+                mc_run_args['dmu']      = delta_mu
+                mc_run_args['repeat']   = i
+                mc_run_args['file_idx'] = count
+                mc_run_args['ref']      = row['ref']
+                mc_run_args['pot']      = row['pot']
 
-    args = {
-        'ce_df':                df,
-        'init_type':            init_type,
-        'ads_species':          ['Br', 'X'],
-        'size':                 18,
-        'temperature':          300.,
-        'prob_threshold':       0.75,
-        'steps':                n_steps,
-        'processes':            n_cores,
-        'potential':            np.round(0., 3),
-        'out_dir':              out_dir,
-        'n_repeats':            1,
-        'log_file':             f'{out_dir}/log.json'
-    }
-    run_batch_mcs(**args)
-    return None
+                with open(log_file, 'a') as f:
+                    json.dump(mc_run_args, f)
+                    f.write('\n')
+                args.append(mc_run_args)
+                count += 1
 
-if __name__ == '__main__':
-    main()
+        # step 4: Define a Pool object with the desired number of processes and run
+        pool        = Pool(processes=n_cores)
+        results     = pool.map_async(MCRunner.run_single_mc, args)
+        results.get()
+        return None
